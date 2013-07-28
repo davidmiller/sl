8b5bc156e882d86246cced0ff08dbcff328f54df
David Miller <david@deadpansincerity.com>
2013-07-28 21:50:12 +0100

    Add Crime topic and dataset.

--- datasets/crime/csew/scripts/clean.r
+++ datasets/crime/csew/scripts/clean.r
@@ -7,8 +7,8 @@ data <- paste0(here, "data/")
 total.crime <- read.xls("http://www.ons.gov.uk/ons/rel/crime-stats/crime-statistics/period-ending-december-2012/chd-figure-1-and-3.xls")
 csew <- data.frame(t(total.crime[2:4,2:32]))
 colnames(csew) <- c("year", "csew.crime", "recorded.crime")
-csew$csew.crime <- as.numeric(gsub(",", "", csew$csew.crime))
-csew$recorded.crime <- as.numeric(gsub(",", "", csew$recorded.crime))
+csew$csew.crime <- as.numeric(gsub(",", "", csew$csew.crime)) * 1000
+csew$recorded.crime <- as.numeric(gsub(",", "", csew$recorded.crime)) * 1000
 write.csv(csew, file=paste0(data, "csew.total.csv"), row.names=FALSE)
 
 violent.crime <- read.xls("http://www.ons.gov.uk/ons/rel/crime-stats/crime-statistics/period-ending-december-2012/chd-figure-4.xls")
@@ -17,3 +17,4 @@ colnames(violence) <- c("year", "nonsense", "crimes")
 violence <- subset(violence, select=c("year", "crimes"))
 violence$crimes <- as.numeric(gsub(",", "", violence$crimes)) * 1000
 write.csv(violence, file=paste0(data, "csew.violent.crimes.csv"), row.names=FALSE)
+
